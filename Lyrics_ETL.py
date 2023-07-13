d1a9792c51159cba531611e9e52cffba7c623563
Ben <ben.marsh304@gmail.com>
2023-07-13 12:07:20 -0700

    Continued work on Lyrics_ETL.py to extract data and insert into database. Added SQL helper functions.

--- Lyrics_ETL.py
+++ Lyrics_ETL.py
@@ -1,9 +1,28 @@
 import json
 import re
 import os
+from helper_functions import create_connection, execute_query, execute_read_query
 
 # The data folder contains the downloaded JSON files for every album. For each file, this will extract the lyrics for each track,
 # do some preliminary cleaning, and save it into the database.
+
+# Connect to SQL database
+connection = create_connection('lyrics_nlp.sqlite')
+
+# Create table for song lyrics
+create_lyrics_table = '''
+CREATE TABLE IF NOT EXISTS lyrics (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    song_title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    album TEXT NOT NULL,
+    release_year INTEGER
+    song_lyrics TEXT NOT NULL
+);
+'''
+execute_query(connection, create_lyrics_table)
+
+# Extract data from JSON files
 directory = 'data'
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
@@ -13,7 +32,8 @@ for filename in os.listdir(directory):
         artist = album['artist']['name']
         album_title = album['name']
         year = album['release_date_components']['year']
-        print(f'{album_title} by {artist}, released in {year}')
+        # **VALIDATION**
+        # print(f'{album_title} by {artist}, released in {year}')
 
         # Find each track and extract lyrics
         for track in album['tracks']:
@@ -23,9 +43,19 @@ for filename in os.listdir(directory):
             first_line = r'^.*?\n'
             lyrics = re.sub(first_line, '', lyrics, count=1)
 
-            brackets = r'\[.*?\]'
-            lyrics = re.sub(brackets, '', lyrics)
+            structure_labels = r'\[.*?\]'
+            lyrics = re.sub(structure_labels, '', lyrics)
             
-            lyrics = lyrics[:-5]
-
+            embed_tag = r'\d*Embed'
+            lyrics = re.sub(embed_tag, '', lyrics)
+            
             # Load track info and lyrics into database.
+            insert_track = f'''
+            INSERT INTO
+                lyrics (song_title, artist, album, release_year, song_lyrics)
+            VALUES
+                ({song_title}, {artist}, {album_title}, {year}, {lyrics});
+            '''
+            execute_query(connection, insert_track)
+
+# Create Artist Table
